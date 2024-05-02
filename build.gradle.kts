90c83ce89509c0f0dee2b10c1bab502d6fc21a85
PresentKim <debe3721@gmail.com>
2024-05-02 17:38:55 +0900

    :tada: Init plugin base

---
+++ build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+    id("io.github.goooler.shadow") version "8.1.5"
+}
+
+group = "kim.present.wdpe.unicodefont.addon"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://repo.waterdog.dev/snapshots")
+    }
+    maven {
+        name = "opencollab-repo-snapshots-mirror"
+        url = uri("https://repo.minjae.dev/opencollab-snapshots-mirror")
+    }
+    maven {
+        url = uri("https://repo.opencollab.dev/maven-snapshots")
+    }
+    maven {
+        url = uri("https://repo.opencollab.dev/maven-releases")
+    }
+    maven {
+        name = "jitpack"
+        url = uri("https://jitpack.io")
+    }
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    compileOnly("dev.waterdog.waterdogpe:waterdog:2.0.0-SNAPSHOT")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks {
+    shadowJar {
+        archiveClassifier.set("")
+    }
+    compileJava {
+        sourceCompatibility = JavaVersion.VERSION_21.toString()
+    }
+    java {
+        sourceCompatibility = JavaVersion.VERSION_21
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(21))
+        }
+    }
+    compileKotlin {
+        kotlinOptions {
+            jvmTarget = JavaVersion.VERSION_21.toString()
+        }
+    }
+}
---
+++ src/main/kotlin/kim/present/wdpe/unicodefont/addon/UnicodeFontAddon.kt
@@ -0,0 +1,11 @@
+package kim.present.wdpe.unicodefont.addon
+
+import dev.waterdog.waterdogpe.plugin.Plugin
+
+class UnicodeFontAddon : Plugin() {
+
+    override fun onStartup() = logger.info("UnicodeFontAddon has been started!")
+
+    override fun onEnable() = logger.info("UnicodeFontAddon has been enabled!")
+
+}
